ed165d75165e2ab6dee33f28ae86ba1c7ebdb084
Brandelan <Bmdela@gmail.com>
2022-12-30 17:15:24 -0800

    Added a script to backup dokiwiki

--- wiki-backup-script.sh
+++ wiki-backup-script.sh
@@ -5,61 +5,61 @@ start_m=`date +%M`
 start_s=`date +%S`
 now=$(date +"%m_%d_%Y-%H_%M")
 
-LOGFILE="/volume1/Brandon/backups/docker/plex/${now}_Plex-Log.txt"
+LOGFILE="/volume1/Brandon/backups/docker/dokuwiki/${now}_Log.txt"
 echo "Script start: $start_m:$start_s" >> $LOGFILE 2>%1
 
-plex_library_dir="/volume1/docker/plex/config/Library/Application Support/Plex Media Server/"
-backup_dir="/volume1/Brandon/backups/docker/plex"
+wiki_library_dir="/volume1/docker/dokuwiki/dokuwiki/"
+backup_dir="/volume1/Brandon/backups/docker/dokuwiki"
 num_backups_to_keep=2
 
 # Stop the container
-docker stop plex
-echo "Stopping plex" >> $LOGFILE 2>%1
+docker stop dokuwiki
+echo "Stopping dokuwiki" >> $LOGFILE 2>%1
 
 # wait 30 seconds
 sleep 30
 
 # Get the state of the docker
-plex_running=`docker inspect -f '{{.State.Running}}' plex`
-echo "Plex running: $plex_running" >> $LOGFILE 2>%1
+wiki_running=`docker inspect -f '{{.State.Running}}' dokuwiki`
+echo "Dokuwiki running: $wiki_running" >> $LOGFILE 2>%1
 
 # If the container is still running retry 5 times
 fail_counter=0
-while [ "$plex_running" = "true" ];
+while [ "$wiki_running" = "true" ];
 do
     fail_counter=$((fail_counter+1))
-    docker stop plex
-    echo "Stopping Plex attempt #$fail_counter" >> $LOGFILE 2>%1
+    docker stop dokuwiki
+    echo "Stopping Dokuwiki attempt #$fail_counter" >> $LOGFILE 2>%1
     sleep 30
-    plex_running=`docker inspect -f '{{.State.Running}}' plex`
+    wiki_running=`docker inspect -f '{{.State.Running}}' dokuwiki`
     # Exit with an error code if the container won't stop
-    # Restart plex and report a warning to the Unraid GUI
+    # Restart dokuwiki and report a warning to the Unraid GUI
     if (($fail_counter == 5));
     then
-        echo "Plex failed to stop. Restarting container and exiting" >> $LOGFILE 2>%1
-        docker start plex
-        echo "Plex Backup failed. Failed to stop container for backup." >> $LOGFILE 2>%1
+        echo "Dokuwiki failed to stop. Restarting container and exiting" >> $LOGFILE 2>%1
+        docker start dokuwiki
+        echo "Dokuwiki Backup failed. Failed to stop container for backup." >> $LOGFILE 2>%1
         exit 1
     fi
 done
 
 # Once the container is stopped, backup the Application Support directory and restart the container
 # The tar command shows progress
-if [ "$plex_running" = "false" ]
+if [ "$wiki_running" = "false" ]
 then
-    echo "Compressing and backing up Plex at ${plex_library_dir}" >> $LOGFILE 2>%1
-    cd ${plex_library_dir}
-    #tar -czf - Application\ Support/ -P | pv -s $(du -sb Application\ Support/ | awk '{print $1}') | gzip > $backup_dir/plex_backup_$now.tar.gz
-    tar --exclude='Cache' --exclude='Crash Reports'  --exclude='Diagnostics' --exclude='Logs' --exclude='Updates' -czf $backup_dir/${now}_plex_backup.tar.gz "${plex_library_dir}"
-    echo "Starting Plex" >> $LOGFILE 2>%1
-    docker start plex
+    echo "Compressing and backing up Dokuwiki at ${wiki_library_dir}" >> $LOGFILE 2>%1
+    cd ${wiki_library_dir}
+    #tar -czf - Application\ Support/ -P | pv -s $(du -sb Application\ Support/ | awk '{print $1}') | gzip > $backup_dir/wiki_backup_$now.tar.gz
+    tar -czf $backup_dir/${now}_wiki_backup.tar.gz "${wiki_library_dir}data/pages" "${wiki_library_dir}data/attic" "${wiki_library_dir}data/meta" "${wiki_library_dir}data/media" "${wiki_library_dir}data/media_attic" "${wiki_library_dir}data/media_meta" "${wiki_library_dir}conf" "${wiki_library_dir}lib"
+    echo "Starting Dokuwiki" >> $LOGFILE 2>%1
+    docker start dokuwiki
 fi
 
 # Get the number of files in the backup directory
-num_files=`ls $backup_dir/*_plex_backup.tar.gz | wc -l`
+num_files=`ls $backup_dir/*_wiki_backup.tar.gz | wc -l`
 echo "Number of files in directory: $num_files" >> $LOGFILE 2>%1
 # Get the full path of the oldest file in the directory
-oldest_file=`ls -t $backup_dir/*_plex_backup.tar.gz | tail -1`
+oldest_file=`ls -t $backup_dir/*_wiki_backup.tar.gz | tail -1`
 echo $oldest_file >> $LOGFILE 2>%1
 
 # After the backup, if the number of files is larger than the number of backups we want to keep
@@ -73,10 +73,10 @@ fi
 
 
 # REMOVE THE OLDEST LOG FILES
-num_files=`ls $backup_dir/*_Plex-Log.txt | wc -l`
+num_files=`ls $backup_dir/*_Log.txt | wc -l`
 echo "Number of files in directory: $num_files" >> $LOGFILE 2>%1
 # Get the full path of the oldest file in the directory
-oldest_file=`ls -t $backup_dir/*_Plex-Log.txt | tail -1`
+oldest_file=`ls -t $backup_dir/*_Log.txt | tail -1`
 echo $oldest_file >> $LOGFILE 2>%1
 
 # After the backup, if the number of files is larger than the number of backups we want to keep
@@ -97,7 +97,7 @@ runtime_s=$((end_s-start_s))
 echo "Script runtime: $runtime_m:$runtime_s" >> $LOGFILE 2>%1
 # Push a notification to the Unraid GUI if the backup failed of passed
 if [[ $? -eq 0 ]]; then
-  echo "Plex Backup completed in $runtime" >> $LOGFILE 2>%1
+  echo "Dokuwiki Backup completed in $runtime" >> $LOGFILE 2>%1
 else
-  echo "Plex Backup failed. See log for more details." >> $LOGFILE 2>%1
+  echo "Dokuwiki Backup failed. See log for more details." >> $LOGFILE 2>%1
 fi
